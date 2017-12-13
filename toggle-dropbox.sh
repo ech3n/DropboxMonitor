7aa6e2e50c3f22ed997388ad26cb08260ee73ca3
e.chen <e.chen@users.noreply.github.com>
2017-12-13 16:53:56 +0800

    Refactor logic to fix bug where Dropbox stays open after sleep wake cycle.

--- toggle-dropbox.sh
+++ toggle-dropbox.sh
@@ -1,50 +1,27 @@
-#
-#      toggle-dropbox
-#
-#      Copyright (c) 2016 Edward Chen
-#
-#      toggle-dropbox.sh - start / stop Dropbox agent
-#
-#      This program is free software: you can redistribute it and/or modify
-#      it under the terms of the GNU General Public License as published by
-#      the Free Software Foundation, either version 3 of the License, or
-#      (at your option) any later version.
-#
-#      This program is distributed in the hope that it will be useful,
-#      but WITHOUT ANY WARRANTY; without even the implied warranty of
-#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#      GNU General Public License for more details.
-#
-#      You should have received a copy of the GNU General Public License
-#      along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 #!/bin/bash
-
+# /Volumes contents changed
+# check if Dropbox needs to be killed or restarted
 if [ $1 ]; then
     # Volume name provided
     VOLUME=$1
 else
-    echo "Usage: toggle-dropbox.sh [volume name]"
-    exit 0
+    VOLUME="echen"
 fi
 
 # Is Dropbox running?
-DROPBOX=`pgrep Dropbox`
+DROPBOX=`pgrep Dropbox | awk '{print $1; exit}'`
 MOUNTPOINT=`diskutil info $VOLUME | grep "Mount Point" | awk '{print $3}'`
 
-# Is SD Card Mounted
-if [ $MOUNTPOINT ]; then
-    # $MOUNTPOINT mounted
-    if [ $DROPBOX ]; then
-        # Dropbox is Running - Stop Dropbox
-        killall Dropbox
-    fi
-    # Restart Dropbox
-    open /Applications/Dropbox.app
+if [ -z $DROPBOX ]; then
+  # Dropbox not running
+  if [ $MOUNTPOINT ]; then
+    # Volume mounted start Dropbox
+        open /Applications/Dropbox.app
+  fi
 else
-    # $MOUNTPOINT unmounted
-    if [ $DROPBOX ]; then
-     # Dropbox running - Stop Dropbox
-     killall Dropbox
-    fi
+  # Dropbox running
+  if [ -z $MOUNTPOINT ]; then
+    # Volume not mounted, kill Dropbox
+    killall Dropbox
+  fi
 fi
